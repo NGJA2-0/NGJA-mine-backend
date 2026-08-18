67cdc55957e6e014809df703ad61b9e5d75a58cf
Ivan-6332 <ivanakalanka@gmail.com>
2026-08-18 15:24:55 +0530

    update the 400 error

--- domain/mining_license.go
+++ domain/mining_license.go
@@ -90,7 +90,7 @@ type MechanizedGemMiningLicense struct {
 	ConditionBreachDetails string   `json:"conditionBreachDetails,omitempty" bson:"conditionBreachDetails,omitempty"`
 	OwnershipComplaint     string   `json:"ownershipComplaint,omitempty" bson:"ownershipComplaint,omitempty"`
 	ComplaintDetails       string   `json:"complaintDetails,omitempty" bson:"complaintDetails,omitempty"`
-	ProposedDepth          *float64 `json:"proposedDepth,omitempty" bson:"proposedDepth,omitempty"`
+	ProposedDepth          interface{} `json:"proposedDepth,omitempty" bson:"proposedDepth,omitempty"` // Deprecated: accepts string or number for backward compatibility
 
 	// Mining proposal
 	LandCultivation string `json:"landCultivation,omitempty" bson:"landCultivation,omitempty"`
